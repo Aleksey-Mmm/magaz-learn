4622f7f8a1053615111144e32ed72085169641aa
Alex <alex@proplus.ru>
2020-11-27 11:29:52 +0900

    получение 4-х случайных продуктов

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -11,10 +11,25 @@ class HomeController extends AppController
 
     public function actionIndex()
     {
-        $offers = Product::find()
+        $prod = Product::find()
             ->where(['is_offer' => 1])
-            ->limit(4)
+            //->limit(4)
             ->all();
+
+        //$offers = [];
+        if (count($prod) > 4) {
+            //получить случайные 4 продукта. способ 1
+            shuffle($prod);
+            $offers = array_slice($prod, 0, 4);
+            //способ2
+/*            $kArr = array_rand($prod, 4);
+            foreach ($kArr as $k) {
+                $offers[] = $prod[$k];
+            }*/
+        } else {
+            $offers = $prod;
+        }
+        //debug($offers);
         return $this->render('index', compact('offers'));
     }
 }
