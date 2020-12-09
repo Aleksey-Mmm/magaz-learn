ac5d8a3f70da868344648b25ded913a17b22be34
Alex <alex@proplus.ru>
2020-12-09 14:18:28 +0900

    отправка емайл

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'adminEmail' => 'admin@grocery.com',
+    'senderEmail' => 'noreply@grocery.com',
+    'senderName' => 'Grocery shop mailer',
 ];
